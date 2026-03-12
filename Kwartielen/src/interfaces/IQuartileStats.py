f01b7bcde4a7dadcac41c4beb18792e543515a7e
Omid2831 <mehrabiomid8282@gmail.com>
2026-03-12 22:40:04 +0100

    refactor: improve formatting and clarity in IQuartileStats interface

--- Kwartielen/src/interfaces/IQuartileStats.py
+++ Kwartielen/src/interfaces/IQuartileStats.py
@@ -2,10 +2,12 @@ from abc import ABC, abstractmethod
 
 
 class IQuartileStats(ABC):
-    """Interface for quartile and interquartile range calculations (Interface Segregation)."""
+    """Interface for quartile and interquartile range calculations."""
 
     @abstractmethod
-    def quartile(self, q: int) -> float: pass
+    def quartile(self, q: int) -> float:
+        pass
 
     @abstractmethod
-    def interquartile_range(self) -> float: pass
+    def interquartile_range(self) -> float:
+        pass
