2145e294bc30eab51ce7a37361e8291d86673414
Omid2831 <mehrabiomid8282@gmail.com>
2026-03-26 09:56:54 +0100

    feat: add Device class for managing smart device attributes and representation

---
+++ SmartHouse/devices/device.py
@@ -0,0 +1,16 @@
+class Device:
+
+    def __init__(self, id, device_location, name, device_type, device_model):
+        self.id = id
+        self.device_location = device_location
+        self.name = name
+        self.device_type = device_type
+        self.device_model = device_model
+
+    def __str__(self):
+        return f"""Device(
+                id={self.id},
+                location={self.device_location},
+                name={self.name}, type={self.device_type},
+                model={self.device_model}
+        )"""
