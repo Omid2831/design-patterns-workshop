4fb5528c3d2ffb15b75a0aee6cb37edfda1670da
Omid2831 <mehrabiomid8282@gmail.com>
2026-03-26 10:04:22 +0100

    feat: refactor SmartDoorLock class to inherit from Device and implement ILockable interface

--- SmartHouse/devices/smartdoor-lock.py
+++ SmartHouse/devices/smartdoor-lock.py
@@ -1,19 +1,22 @@
 from SmartHouse.interfaces.ILockable import ILockable
+from SmartHouse.devices.device import Device
+class SmartDoorLock(Device, ILockable):
 
-class SmartDoorLock(ILockable):
-
-    def __init__ (self, name):
-        self.name = name
+    def __init__ (self, id, device_location, name, device_model):
+        super().__init__(id, device_location, name, "Smart Door Lock", device_model)
         self.is_locked = False
 
     def lock(self):
         self.is_locked = True
+        self.device_info()
         print(f"{self.name} is now LOCKED.")
 
     def unlock(self):
         self.is_locked = False
+        self.device_info()
         print(f"{self.name} is now UNLOCKED.")
         
     def is_locked(self) -> bool:
+        self.device_info()
         return self.is_locked
         
